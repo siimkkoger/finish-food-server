a08a59f350fd7f13cb5d252a4e0de6143ef470e3
siimkk <siimkkoger@gmail.com>
2023-09-14 20:54:55 +0200

    Remove AppConfig.java class files.

--- api-gw/src/main/java/com/ffreaky/apigw/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ffreaky.apigw;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-
-
-}
--- food-service/src/main/java/com/ffreaky/foodservice/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ffreaky.foodservice;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-
-
-}
