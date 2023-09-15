561e959bf46fb7f7bbb0d61df0358d363b8360b6
siimkk <siimkkoger@gmail.com>
2023-09-15 20:30:58 +0200

    Test db connection on localhost

--- food-service/src/main/resources/migration/V2023_09_14_21_48_starter.sql
+++ food-service/src/main/resources/migration/V2023_09_14_21_48_starter.sql
@@ -1,8 +1,20 @@
 CREATE TABLE public.test
 (
-    id             serial         NOT NULL PRIMARY KEY,
-    name           boolean        NOT NULL default true,
-    created_at     timestamp      NOT NULL DEFAULT current_timestamp,
-    updated_at     timestamp      NOT NULL DEFAULT current_timestamp,
-    deleted_at     timestamp      NULL
+    id         serial    NOT NULL PRIMARY KEY,
+    name       boolean   NOT NULL default true,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp,
+    deleted_at timestamp NULL
 );
+
+INSERT INTO public.test (id)
+VALUES (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT),
+       (DEFAULT);
