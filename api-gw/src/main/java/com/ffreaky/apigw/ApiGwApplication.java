4a63cadc93f73f157c62e4a492c5c03b3757347c
siimkk <siimkkoger@gmail.com>
2023-09-16 12:18:06 +0200

    Finally figured out docker socket hangup Ö_Ö finally... my eyes...

--- api-gw/src/main/java/com/ffreaky/apigw/ApiGwApplication.java
+++ api-gw/src/main/java/com/ffreaky/apigw/ApiGwApplication.java
@@ -1,13 +1,41 @@
 package com.ffreaky.apigw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class ApiGwApplication {
 
+    static Logger logger = LoggerFactory.getLogger(ApiGwApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGwApplication.class, args);
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    logger.info("api-gw-info");
+                    logger.debug("api-gw-debug");
+                    logger.error("api-gw-error");
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    @GetMapping(value = "/api-gw/test")
+    boolean test() {
+        return true;
     }
 
 }
--- food-service/src/main/java/com/ffreaky/foodservice/FoodServiceApplication.java
+++ food-service/src/main/java/com/ffreaky/foodservice/FoodServiceApplication.java
@@ -1,13 +1,38 @@
 package com.ffreaky.foodservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class FoodServiceApplication {
 
+    static Logger logger = LoggerFactory.getLogger(FoodServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(FoodServiceApplication.class, args);
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    logger.info("food-service-info");
+                    logger.debug("food-service-debug");
+                    logger.error("food-service-error");
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 
 }
