dc371b5dff25e6cab8cfff29141a089100a555b5
siimkk <siimkkoger@gmail.com>
2023-09-18 18:14:35 +0200

    Test out Intellij http client

--- food-service/src/main/java/com/ffreaky/foodservice/controller/TestController.java
+++ food-service/src/main/java/com/ffreaky/foodservice/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.ffreaky.foodservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -12,5 +10,13 @@ public class TestController {
     @GetMapping(value = "/test")
     boolean test() {
         return true;
+    }
+
+    @PostMapping(value = "/test")
+    boolean testPostDto(@RequestBody TestDto testDto) {
+        return testDto.id() == 0 && testDto.name().equals("testname");
+    }
+
+    private record TestDto(int id, String name) {
     }
 }
