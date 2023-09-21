85f27aafb22644ad51bc57af82b1323970875997
siimkk <siimkkoger@gmail.com>
2023-09-21 23:20:36 +0200

    Add dummy data to database

--- food-service/src/main/resources/migration/V2023_09_21_21_35_starter.sql
+++ food-service/src/main/resources/migration/V2023_09_21_21_35_starter.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user
+CREATE TABLE public.user
 (
     id           SERIAL PRIMARY KEY,
     first_name   VARCHAR(255) NOT NULL,
@@ -10,13 +10,13 @@ CREATE TABLE user
 
     created_at   timestamp    NOT NULL DEFAULT current_timestamp,
     updated_at   timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at   timestamp NULL
+    deleted_at   timestamp    NULL
 );
 
-CREATE TABLE user_address
+CREATE TABLE public.user_address
 (
     id             SERIAL PRIMARY KEY,
-    user_id        INT          NOT NULL REFERENCES user (id),
+    user_id        INT          NOT NULL REFERENCES public.user (id),
     street_address VARCHAR(255) NOT NULL,
     city           VARCHAR(255) NOT NULL,
     state          VARCHAR(255) NOT NULL,
@@ -27,24 +27,62 @@ CREATE TABLE user_address
 
     created_at     timestamp    NOT NULL DEFAULT current_timestamp,
     updated_at     timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at     timestamp NULL
+    deleted_at     timestamp    NULL
 );
 
-CREATE TABLE user_payment
+CREATE TABLE public.payment_provider
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+
+    created_at  timestamp    NOT NULL DEFAULT current_timestamp,
+    updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
+    deleted_at  timestamp    NULL
+);
+
+CREATE TABLE public.user_payment
 (
     id           SERIAL PRIMARY KEY,
-    user_id      INT          NOT NULL REFERENCES user (id),
-    provider_id  INT          NOT NULL REFERENCES payment_provider (id),
+    user_id      INT          NOT NULL REFERENCES public.user (id),
+    provider_id  INT          NOT NULL REFERENCES public.payment_provider (id),
     payment_type VARCHAR(255) NOT NULL,
     account_no   VARCHAR(255) NOT NULL,
-    expiry_date  DATE         NOT NULL,
+    expiry_date  DATE,
 
     created_at   timestamp    NOT NULL DEFAULT current_timestamp,
     updated_at   timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at   timestamp NULL
+    deleted_at   timestamp    NULL
 );
 
-CREATE TABLE payment_provider
+CREATE TABLE public.product_provider_type
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+
+    created_at timestamp    NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp    NOT NULL DEFAULT current_timestamp,
+    deleted_at timestamp    NULL
+);
+
+CREATE TABLE public.product_provider
+(
+    id               SERIAL PRIMARY KEY,
+    name             VARCHAR(255) NOT NULL,
+    provider_type_id INT          NOT NULL REFERENCES public.product_provider_type (id),
+    description      VARCHAR(255) NOT NULL,
+    address          VARCHAR(255) NOT NULL,
+    phone            VARCHAR(255) NOT NULL,
+    email            VARCHAR(255) NOT NULL,
+    website          VARCHAR(255) NOT NULL,
+    image            VARCHAR(255) NOT NULL,
+
+    created_at       timestamp    NOT NULL DEFAULT current_timestamp,
+    updated_at       timestamp    NOT NULL DEFAULT current_timestamp,
+    deleted_at       timestamp    NULL
+);
+
+CREATE TABLE public.product_category
 (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(255) NOT NULL,
@@ -52,88 +90,25 @@ CREATE TABLE payment_provider
 
     created_at  timestamp    NOT NULL DEFAULT current_timestamp,
     updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
+    deleted_at  timestamp    NULL
 );
 
-CREATE TABLE user_wishlist
-(
-    id         SERIAL PRIMARY KEY,
-    user_id    INT       NOT NULL REFERENCES user (id),
-
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    updated_at timestamp NOT NULL DEFAULT current_timestamp,
-    deleted_at timestamp NULL
-);
-
-CREATE TABLE user_wishlist_item
-(
-    id               SERIAL PRIMARY KEY,
-    user_wishlist_id INT       NOT NULL REFERENCES user_wishlist (id),
-    product_id       INT       NOT NULL REFERENCES product (id),
-
-    created_at       timestamp NOT NULL DEFAULT current_timestamp,
-    updated_at       timestamp NOT NULL DEFAULT current_timestamp,
-    deleted_at       timestamp NULL
-);
-
-CREATE TABLE product
+CREATE TABLE public.product
 (
     id                  SERIAL PRIMARY KEY,
-    product_provider_id INT            NOT NULL REFERENCES product_provider (id),
-    inventory_id        INT            NOT NULL REFERENCES inventory (id),
-    name                VARCHAR(255)   NOT NULL,
-    description         VARCHAR(1000)  NOT NULL,
-    price               DECIMAL(19, 4) NOT NULL,
-    discount            INT            NOT NULL REFERENCES discount (id),
-    image               VARCHAR(255)   NOT NULL,
-    pickup_time         DATE           NOT NULL,
+    product_category_id INT       NOT NULL REFERENCES public.product_category (id),
+    product_provider_id INT       NOT NULL REFERENCES public.product_provider (id),
+    pickup_time         DATE      NOT NULL,
 
-    created_at          timestamp      NOT NULL DEFAULT current_timestamp,
-    updated_at          timestamp      NOT NULL DEFAULT current_timestamp,
+    created_at          timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at          timestamp NOT NULL DEFAULT current_timestamp,
     deleted_at          timestamp NULL
 );
 
-CREATE TABLE inventory
-(
-    id         SERIAL PRIMARY KEY,
-    quantity   INT       NOT NULL,
-
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
-    updated_at timestamp NOT NULL DEFAULT current_timestamp,
-    deleted_at timestamp NULL
-);
-
-CREATE TABLE product_provider
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    address     VARCHAR(255) NOT NULL,
-    phone       VARCHAR(255) NOT NULL,
-    email       VARCHAR(255) NOT NULL,
-    website     VARCHAR(255) NOT NULL,
-    image       VARCHAR(255) NOT NULL,
-
-    created_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
-);
-
-CREATE TABLE product_category
-(
-    id          SERIAL PRIMARY KEY,
-    parent_id   INT REFERENCES product_category (id),
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-
-    created_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
-);
-
-CREATE TABLE discount
+CREATE TABLE public.discount
 (
     id               SERIAL PRIMARY KEY,
+    product_id       INT          NOT NULL REFERENCES public.product (id),
     name             VARCHAR(255) NOT NULL,
     discount_percent DECIMAL      NOT NULL,
     description      VARCHAR(255) NOT NULL,
@@ -141,26 +116,37 @@ CREATE TABLE discount
 
     created_at       timestamp    NOT NULL DEFAULT current_timestamp,
     updated_at       timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at       timestamp NULL
+    deleted_at       timestamp    NULL
 );
 
-CREATE TABLE shopping_session
+CREATE TABLE public.user_favorite_providers
+(
+    id                  SERIAL PRIMARY KEY,
+    user_id             INT       NOT NULL REFERENCES public.user (id),
+    product_provider_id INT       NOT NULL REFERENCES public.product_provider (id),
+
+    created_at          timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at          timestamp NOT NULL DEFAULT current_timestamp,
+    deleted_at          timestamp NULL
+);
+
+CREATE TABLE public.shopping_session
 (
     id         SERIAL PRIMARY KEY,
-    user_id    INT            NOT NULL REFERENCES user (id),
+    user_id    INT            NOT NULL REFERENCES public.user (id),
     total_cost DECIMAL(19, 4) NOT NULL,
     status     VARCHAR(255)   NOT NULL,
 
     created_at timestamp      NOT NULL DEFAULT current_timestamp,
     updated_at timestamp      NOT NULL DEFAULT current_timestamp,
-    deleted_at timestamp NULL
+    deleted_at timestamp      NULL
 );
 
-CREATE TABLE cart_item
+CREATE TABLE public.cart_item
 (
     id                  SERIAL PRIMARY KEY,
-    shopping_session_id INT       NOT NULL REFERENCES shopping_session (id),
-    product_id          INT       NOT NULL REFERENCES product (id),
+    shopping_session_id INT       NOT NULL REFERENCES public.shopping_session (id),
+    product_id          INT       NOT NULL REFERENCES public.product (id),
     quantity            INT       NOT NULL,
 
     created_at          timestamp NOT NULL DEFAULT current_timestamp,
@@ -168,11 +154,22 @@ CREATE TABLE cart_item
     deleted_at          timestamp NULL
 );
 
-CREATE TABLE order_items
+CREATE TABLE public.order_details
 (
     id         SERIAL PRIMARY KEY,
-    order_id   INT       NOT NULL REFERENCES order_details (id),
-    product_id INT       NOT NULL REFERENCES product (id),
+    user_id    INT            NOT NULL REFERENCES public.user (id),
+    total_cost DECIMAL(19, 4) NOT NULL,
+
+    created_at timestamp      NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp      NOT NULL DEFAULT current_timestamp,
+    deleted_at timestamp      NULL
+);
+
+CREATE TABLE public.order_items
+(
+    id         SERIAL PRIMARY KEY,
+    order_id   INT       NOT NULL REFERENCES public.order_details (id),
+    product_id INT       NOT NULL REFERENCES public.product (id),
     quantity   INT       NOT NULL,
 
     created_at timestamp NOT NULL DEFAULT current_timestamp,
@@ -180,73 +177,68 @@ CREATE TABLE order_items
     deleted_at timestamp NULL
 );
 
-CREATE TABLE order_details
+CREATE TABLE public.payment_details
+(
+    id               UUID PRIMARY KEY,
+    amount           DECIMAL(19, 4) NOT NULL,
+    status           VARCHAR(255)   NOT NULL,
+    provider_id      INT            NOT NULL REFERENCES public.payment_provider (id),
+    order_details_id INT            NOT NULL REFERENCES public.order_details (id),
+
+    created_at       timestamp      NOT NULL DEFAULT current_timestamp,
+    updated_at       timestamp      NOT NULL DEFAULT current_timestamp,
+    deleted_at       timestamp      NULL
+);
+
+CREATE TABLE public.food_category
 (
     id         SERIAL PRIMARY KEY,
-    user_id    INT            NOT NULL REFERENCES user (id),
-    total_cost DECIMAL(19, 4) NOT NULL,
-    payment_id UUID           NOT NULL REFERENCES payment_details (id),
+    name       VARCHAR(255) NOT NULL,
 
-    created_at timestamp      NOT NULL DEFAULT current_timestamp,
-    updated_at timestamp      NOT NULL DEFAULT current_timestamp,
-    deleted_at timestamp NULL
+    created_at timestamp    NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp    NOT NULL DEFAULT current_timestamp,
+    deleted_at timestamp    NULL
 );
 
-CREATE TABLE payment_details
-(
-    id          UUID PRIMARY KEY,
-    amount      DECIMAL(19, 4) NOT NULL,
-    status      VARCHAR(255)   NOT NULL,
-    provider_id INT            NOT NULL REFERENCES payment_provider (id),
-
-    created_at  timestamp      NOT NULL DEFAULT current_timestamp,
-    updated_at  timestamp      NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
-);
-
-CREATE TABLE food
+CREATE TABLE public.food
 (
     id                   SERIAL PRIMARY KEY,
-    product_id           INT           NOT NULL REFERENCES product (id),
-    food_category_id     INT           NOT NULL REFERENCES food_category (id),
-    dietary_restrictions VARCHAR(1000) NOT NULL,
+    product_id           INT            NOT NULL REFERENCES public.product (id),
+    food_category_id     INT            NOT NULL REFERENCES public.food_category (id),
+    name                 VARCHAR(255)   NOT NULL,
+    description          VARCHAR(1000)  NOT NULL,
+    price                DECIMAL(19, 4) NOT NULL,
+    image                VARCHAR(255)   NOT NULL,
+    dietary_restrictions VARCHAR(1000)  NOT NULL,
 
-    created_at           timestamp     NOT NULL DEFAULT current_timestamp,
-    updated_at           timestamp     NOT NULL DEFAULT current_timestamp,
-    deleted_at           timestamp NULL
+    created_at           timestamp      NOT NULL DEFAULT current_timestamp,
+    updated_at           timestamp      NOT NULL DEFAULT current_timestamp,
+    deleted_at           timestamp      NULL
 );
 
-CREATE TABLE food_category
+CREATE TABLE public.clothes_category
 (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
 
-    created_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
+    created_at timestamp    NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp    NOT NULL DEFAULT current_timestamp,
+    deleted_at timestamp    NULL
 );
 
-CREATE TABLE clothes
+CREATE TABLE public.clothes
 (
     id                  SERIAL PRIMARY KEY,
-    product_id          INT          NOT NULL REFERENCES product (id),
-    clothes_category_id INT          NOT NULL REFERENCES clothes_category (id),
-    size                VARCHAR(255) NOT NULL,
-    color               VARCHAR(255) NOT NULL,
+    product_id          INT            NOT NULL REFERENCES public.product (id),
+    clothes_category_id INT            NOT NULL REFERENCES public.clothes_category (id),
+    name                VARCHAR(255)   NOT NULL,
+    description         VARCHAR(1000)  NOT NULL,
+    price               DECIMAL(19, 4) NOT NULL,
+    image               VARCHAR(255)   NOT NULL,
+    size                VARCHAR(255)   NOT NULL,
+    color               VARCHAR(255)   NOT NULL,
 
-    created_at          timestamp    NOT NULL DEFAULT current_timestamp,
-    updated_at          timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at          timestamp NULL
-);
-
-CREATE TABLE clothes_category
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-
-    created_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    updated_at  timestamp    NOT NULL DEFAULT current_timestamp,
-    deleted_at  timestamp NULL
+    created_at          timestamp      NOT NULL DEFAULT current_timestamp,
+    updated_at          timestamp      NOT NULL DEFAULT current_timestamp,
+    deleted_at          timestamp      NULL
 );
